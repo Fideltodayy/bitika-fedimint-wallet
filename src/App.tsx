930841ccc12ef237bc704526a80ab7e44e06c4b6
Fideltodayy <fidelotieno11@gmail.com>
2025-03-10 10:52:09 +0300

    Setup fedimint wallet to receive and send ln payments

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { WalletApp } from '@/components/wallet-app'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello World</h1>
+      <WalletApp />
+    </>
+  )
+}
+
+export default App
